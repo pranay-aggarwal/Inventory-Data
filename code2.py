27c59684ffcefaa6f7093264c9c3af5472c8f0f7
pranay-aggarwal <85791773+pranay-aggarwal@users.noreply.github.com>
2023-05-20 23:53:05 +0530

    Create code2.py

---
+++ code2.py
@@ -0,0 +1,120 @@
+### Without CSV file for storing data
+
+
+
+inventory = []
+password = 1234
+
+while True:
+    print()
+    print("==========inventory Tracker==========")
+    print("1. Open User Options")
+    print("2. Open Admin Settings")
+    print("3. Quit")
+    print("=====================================")
+    
+    choice = input("Enter your choice: ")
+
+##########################################################################
+
+    if choice == "1":
+        while True: 
+            print()
+            print("=============User Options============")
+            print("1. Add Product")
+            print("2. View Total Price of products")
+            print("3. View Prices by product")
+            print("4. Return to main menu")
+            print("=====================================")
+
+            choice = input("Enter your choice: ")
+
+            if choice == "1":
+                brand = input("Enter the Brand name: ")
+                product = input("Enter the product name: ")
+                price = float(input("Enter the price: "))
+                inventory.append((brand, (product, price)))
+                print("Product added successfully!")
+                print("=====================================")
+                print()
+
+            elif choice == "2":
+                sum = 0
+                brand = input("Enter the Brand: ")
+                for i in inventory:
+                    if i[0] == brand:
+                        sum += i[1][1]
+                print("Total cost of this brand: ", sum)
+                print("=====================================")
+                print()
+
+            elif choice == "3":
+                print("Prices per Product:")
+                brand = input("Enter the Brand: ")
+                for i in inventory:
+                    if i[0] == brand:
+                        print(i[1][0], ":", i[1][1])
+                print("=====================================")
+                print()
+
+            elif choice == "4":
+                print("=====================================")
+                break
+
+##########################################################################
+
+    elif choice == "2":
+        while True:
+            print()
+            print("============Admin Settings===========")
+            print("1. Print all the Products")
+            print("2. Change password")
+            print("3. Return to main menu")
+            print("=====================================")
+
+            choice = input("Enter your choice: ")
+
+            if choice == "1":
+                print("Inventory:")
+                admin= int(input("Enter the Admin Password: "))
+                if admin == password:
+                    for i in inventory:
+                        print(i[0])
+                        print(i[1][0], ":", i[1][1])
+                        print()
+                    print("=====================================")
+                    print()
+                else:
+                    print("Access Denied")
+                    print("=====================================")
+                    print()
+                        
+
+            elif choice == "2":
+                print("Change Password:")
+                admin= int(input("Enter the Current Admin Password: "))
+                if admin == password:
+                    password = int(input("Enter the New Admin Password: "))
+                    print("Password Changed successfully")
+                    print("NOTE: Running the program again will") 
+                    print("reset the password to default")
+                else:
+                    print("Access Denied")
+                print("=====================================")
+                print()
+            
+            elif choice == "3":
+                print("=====================================")
+                break
+
+##########################################################################
+
+    elif choice == "3":
+        print("Thank you for using Inventory Tracker!")
+        print("=====================================")
+        break
+
+    else:
+        print("Invalid choice. Please try again.")
+        print("=====================================")
+        print()
